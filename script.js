1543c75c2a0f2544238208234aa86517e773816b
mbrijmohan <mbrijmohan816@gmail.com>
2024-08-23 11:03:28 +0530

    Font Update and JavaScript Added

---
+++ script.js
@@ -0,0 +1,10 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word)=>{
+    let letters = word.textContent.split("");
+    word.textContent="";
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+    })
+
+    <--https://www.youtube.com/watch?v=fZm4gJDY_zY-->
+});
